43f558fc4e2e37ac16f3f0cf749958c505839ebc
Buki <Bukithompson@gmail.com>
2017-05-05 22:43:57 +0100

    sorted routes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('JS loaded');
+$(() => {
+  // const $about = $('#about');
+  // const $aboutBox = $('#about-box');
+  //
+  //
+  // $about.click(function(){
+  //   $aboutBox.removeClass('hidden');
+  // });
+});
+  // </script>
+  // </head>
+  // <body>
+  //
+  // <p>This is a paragraph.</p>
+  //
+  // <button>Toggle between hide() and show()</button>
