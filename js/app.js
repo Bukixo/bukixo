f695ff4fed99a03927da1ff3fff1e5053c2e743f
Buki <Bukithompson@gmail.com>
2017-05-09 20:27:10 +0100

    toggled contacts

--- js/app.js
+++ js/app.js
@@ -1,16 +1,37 @@
 $(() => {
-  const $about = $('#about');
-  const $aboutBox = $('#about-box');
+  // const $about = $('#about');
+  // const $aboutBox = $('#about-box');
+  const $linkedinSvg = $('#linkedinSvg');
+  const $linkedin = $('#linkedin');
+  const $githubSvg = $('#githubSvg');
+  const $github = $('#github');
+  const $emailSvg = $('#emailSvg');
+  const $email = $('#email');
+  const $contactInfo = $('.contactInfo');
+  const $contacts = $('.contacts');
 
+  $githubSvg.addClass('animated slideInDown');
+  $linkedinSvg.addClass('animated slideInDown');
+  $emailSvg.addClass('animated slideInDown');
 
-  $about.click(function(){
-    $aboutBox.toggle();
+  $linkedinSvg.on('click', function () {
+    $contactInfo.text(function(text) {
+      return text === '' || text.length !== 0 ? 'www.linkedin.com/bukithompson' : '';
+    }).addClass('animated fadeIn');
   });
+
+  $emailSvg.on('click', function () {
+    $contactInfo.text(function(text) {
+      return text === '' || text.length !== 0 ? 'Bukithompson@gmail.com' : '';
+    }).addClass('animated slideInLeft');
+  });
+
+  $githubSvg.on('click', function () {
+    $contactInfo.text(function(text) {
+      return text === '' || text.length !== 0 ? 'www.Github.com/Bukixo' : '';
+    }).addClass('animated bounceInRight');
+  });
+
+
+
 });
-  // </script>
-  // </head>
-  // <body>
-  //
-  // <p>This is a paragraph.</p>
-  //
-  // <button>Toggle between hide() and show()</button>
