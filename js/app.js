11720942a9d0ad8b118a281a0ee7c8de98f9be5b
Buki <Bukithompson@gmail.com>
2017-05-09 22:40:11 +0100

    changed backgound colur fade

--- js/app.js
+++ js/app.js
@@ -1,18 +1,35 @@
 $(() => {
+  const $titleBox = $('.title-box');
   // const $about = $('#about');
-  // const $aboutBox = $('#about-box');
+  const $aboutBox = $('.about-box');
   const $linkedinSvg = $('#linkedinSvg');
-  const $linkedin = $('#linkedin');
+  // const $linkedin = $('#linkedin');
   const $githubSvg = $('#githubSvg');
-  const $github = $('#github');
+  // const $github = $('#github');
   const $emailSvg = $('#emailSvg');
-  const $email = $('#email');
+  // const $email = $('#email');
   const $contactInfo = $('.contactInfo');
-  const $contacts = $('.contacts');
+  // const $contacts = $('.contacts');
+  const $home = $('.home');
+  const $fabric = $('fabric');
+  const $wanderful = $('.wanderful');
+  const $ed = $('.ed');
+  const $shuku = $('.shuku');
+  const $simon = $('.simon');
+
+  backgroundChange();
+
+
+
 
   $githubSvg.addClass('animated slideInDown');
   $linkedinSvg.addClass('animated slideInDown');
   $emailSvg.addClass('animated slideInDown');
+  $fabric.addClass('animated slideInDown');
+  $wanderful.addClass('animated slideInDown');
+  $ed.addClass('animated slideInDown');
+  $shuku.addClass('animated slideInDown');
+  $simon.addClass('animated slideInDown');
 
   $linkedinSvg.on('click', function () {
     $contactInfo.text(function(text) {
@@ -33,5 +50,19 @@ $(() => {
   });
 
 
+///////////////////////////////////////////////
 
-});
+
+  function changeColor(selector, colors) {
+    var curCol = 0;
+    if (curCol === colors.length) curCol = 0;
+    $(selector).css('background-color', colors[curCol]);
+    curCol++;
+
+  }
+
+  function backgroundChange(){
+    changeColor($home, ['Grey']);
+  }
+
+}); ////////////window closing tag
