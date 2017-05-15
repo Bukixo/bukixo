bd462035080b5d9ac97a0035019c590f1f9e1721
Buki <Bukithompson@gmail.com>
2017-05-15 13:54:45 +0100

    removed contacts and added it on to the footer

--- js/app.js
+++ js/app.js
@@ -1,54 +1,18 @@
 $(() => {
-  const $titleBox = $('.title-box');
-  // const $about = $('#about');
-  const $aboutBox = $('.about-box');
-  const $linkedinSvg = $('#linkedinSvg');
-  // const $linkedin = $('#linkedin');
-  const $githubSvg = $('#githubSvg');
-  // const $github = $('#github');
-  const $emailSvg = $('#emailSvg');
-  // const $email = $('#email');
-  const $contactInfo = $('.contactInfo');
-  // const $contacts = $('.contacts');
+  // const $titleBox = $('.title-box');
+  // // const $about = $('#about');
+  // const $aboutBox = $('.about-box');
+  // const $contactInfo = $('.contactInfo');
+  // // const $contacts = $('.contacts');
   const $home = $('.home');
-  const $fabric = $('fabric');
-  const $wanderful = $('.wanderful');
-  const $ed = $('.ed');
-  const $shuku = $('.shuku');
-  const $simon = $('.simon');
+  // const $fabric = $('fabric');
+  // const $wanderful = $('.wanderful');
+  // const $ed = $('.ed');
+  // const $shuku = $('.shuku');
+  // const $simon = $('.simon');
   const $links = $('.projects a');
 
   backgroundChange();
-
-
-
-
-  $githubSvg.addClass('animated slideInDown');
-  $linkedinSvg.addClass('animated slideInDown');
-  $emailSvg.addClass('animated slideInDown');
-  $fabric.addClass('animated slideInDown');
-  $wanderful.addClass('animated slideInDown');
-  $ed.addClass('animated slideInDown');
-  $shuku.addClass('animated slideInDown');
-  $simon.addClass('animated slideInDown');
-
-  $linkedinSvg.on('click', function () {
-    $contactInfo.text(function(text) {
-      return text === '' || text.length !== 0 ? 'www.linkedin.com/bukithompson' : '';
-    }).addClass('animated fadeIn');
-  });
-
-  $emailSvg.on('click', function () {
-    $contactInfo.text(function(text) {
-      return text === '' || text.length !== 0 ? 'Bukithompson@gmail.com' : '';
-    }).addClass('animated slideInLeft');
-  });
-
-  $githubSvg.on('click', function () {
-    $contactInfo.text(function(text) {
-      return text === '' || text.length !== 0 ? 'www.Github.com/Bukixo' : '';
-    }).addClass('animated bounceInRight');
-  });
 
 
 ///////////////////////////////////////////////
